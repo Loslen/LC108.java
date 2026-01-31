b5648df0d1186825b870678fbaa75f9b39610a22
Loslen <113829585+Loslen@users.noreply.github.com>
2026-01-31 23:40:47 +0800

    Create LC108.java

---
+++ LC108.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        //昇順配列の中央要素をルートノードとする
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, lo, mid -1);
+        root.right = dfs(nums, mid + 1, hi);
+        return root;
+    }
+};
